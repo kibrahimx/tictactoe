74e2bbbdacbeb4bf2b795eace43354c88d07fd52
Ibrahim <ibraguim.karsamov@gmail.com>
2023-07-05 12:02:46 +0200

    manque plus que le gameover

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const gameboardGrid = document.getElementById("gameboard");
+const turnElement = document.getElementById("turn");
+let turnSpan = document.getElementById("turn-span");
 
 const Gameboard = (() => {
     let gameboard = [];
@@ -28,23 +30,74 @@ const Gameboard = (() => {
 })();
 
 const Game = (() => {
+    const players = ["Blue", "Red"];
+    let turn = "Blue";
     let gameover = false;
 
     const start = () => {
+        if (!turnElement.style.opacity == 1) {
+            turnElement.style.opacity = 1;
+        }
         Gameboard.render();
+    }
+
+    const clear = () => {
+        gameboardGrid.innerHTML = "";
     }
 
     const handleClick = (event) => {
         console.log(event.target.id.split("-"));
+
+        if (turn == "Blue" && event.target.innerHTML == "") {
+            event.target.innerHTML = "O";
+            event.target.style.display = "flex";
+            event.target.style.justifyContent = "center";
+            event.target.style.alignItems = "center";
+            event.target.style.fontSize = "100px";
+            event.target.style.color = "blue";
+
+            turn = "Red";
+            turnSpan.innerHTML = "Red";
+            turnSpan.style.color = "red";
+        }
+        else if (turn == "Red" && event.target.innerHTML == "") {
+            event.target.innerHTML = "X";
+            event.target.style.display = "flex";
+            event.target.style.justifyContent = "center";
+            event.target.style.alignItems = "center";
+            event.target.style.fontSize = "100px";
+            event.target.style.color = "red";
+
+            turn = "Blue";
+            turnSpan.innerHTML = "Blue";
+            turnSpan.style.color = "blue";
+        }
+
+        Game.checkGameover();
     }
 
+    const checkGameover = () => {
+        // Doit check si ya une ligne de 3 ou égalité
+    }
+    
     return {
         start,
-        handleClick
+        clear,
+        handleClick,
+        checkGameover
     }
 })();
 
 const startButton = document.getElementById("start-btn");
 startButton.addEventListener("click", () => {
-    Game.start();
+    if (!gameboardGrid.hasChildNodes()) {
+        Game.start();
+    }
+})
+
+const clearButton = document.getElementById("clear-btn");
+clearButton.addEventListener("click", () => {
+    if (gameboardGrid.hasChildNodes()) {
+        Game.clear();
+    }
 })
